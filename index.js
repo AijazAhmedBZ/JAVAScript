84982773dd9bf983a9251d89ea05a9fd176083fb
Aijaz Ahmed <aijaz.ahmed.bz@gmail.com>
2023-02-21 18:21:17 +0500

    updated on 21-feb-23

---
+++ index.js
@@ -0,0 +1,106 @@
+// alert("AOA");
+// console.log("Aoa from Aijaz");
+// console.log("AOA via Consol");
+// console.log(34);
+
+// document.write("AOA via Consol");
+// document.write(2+8);
+
+/*var age = 45;
+var nationality = "Pakistani";
+var feePaid = true;
+
+alert(feePaid);
+console.log(age);
+console.log(feePaid);
+
+var age = 45;
+console.log(age);
+var age = "Pakistani";
+console.log(age);
+var age = true;
+console.log(age);*/
+
+//PROMPT
+/*var question ="What is your name?";
+var defaultAnswer = "Aijaz";
+var name = prompt(question, defaultAnswer);
+var question ="What is your age?";
+var age = prompt(question);
+console.log("Name = "+ name);
+console.log("Age = "+ age);
+
+//PROMPT with parseInt
+var question ="What is your name?";
+var defaultAnswer = "Aijaz";
+var name = prompt(question, defaultAnswer);
+var age = prompt("What is your age?");
+var num = 4;
+var sum = parseInt(age)+ num;
+console.log("Name = "+ name);
+console.log("Age = "+ sum);
+
+//PROMPT with parseFloat (for converting decimal string in number)
+var question ="What is your name?";
+var defaultAnswer = "Aijaz";
+var name = prompt(question, defaultAnswer);
+var age = prompt("What is your age?");
+var num = 4;
+var sum = parseFloat(age)+ num;
+console.log("Name = "+ name);
+console.log("Age = "+ sum);
+
+//PROMPT with Number (for converting decimal string in number)
+var question ="What is your name?";
+var defaultAnswer = "Aijaz";
+var name = prompt(question, defaultAnswer);
+var age = prompt("What is your age?");
+var num = 4;
+var sum = Number(age) + num;
+console.log("Name = "+ name);
+console.log("Age = "+ sum);
+
+//Comparison operator
+var a = 5;
+var b = 6;
+console.log(a == b);
+console.log(a != b);
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
+
+//Logical Operators
+//&& (AND)
+var a = 60;
+var b = a > 50 && a < 70;
+alert (b);
+
+var c = 80;
+var d = c > 50 && c < 70;
+alert (d);
+
+//Logical Operators
+//|| (OR)
+var a = 60;
+var b = a > 50 || a < 70;
+console.log(b);
+
+var c = 80;
+var d = c > 50 || c < 70;
+console.log(d);*/
+
+//Logical Operators
+// ! (NOT)
+var a = 60;
+var b = a > 100;
+var c = !b;
+var d = !(a > 100);
+var e = !true;
+
+console.log("A = "+a);
+console.log("B = "+b);
+console.log("C = "+c);
+console.log("D = "+d);
+console.log("E = "+e);
+
